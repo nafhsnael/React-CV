6f861905ffc9bd348b6eba7835316ff4495826cd
Nafhisa Nailah Husnah <133951870+nafhsnael@users.noreply.github.com>
2024-03-04 13:59:09 +0700

    Create kontak.js

---
+++ kontak.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './tampilan.css';
+
+class Kontak extends React.Component {
+    render() {
+        return(
+            <div>
+            <div class="tittle">
+               <h1>Contact Person</h1>
+            </div>
+            <center><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <h4>Nafhisa Nailah Husnah</h4>
+            <h4>Email : nafhisa_husnah_31rpl@student.smktelkom-mlg.sch.id</h4>
+            <h4>No.Telepon : 081805000471</h4>
+            <h4>Instagram : Nafhs.nael</h4>
+            </center>
+            </div>
+        )
+    }
+}
+
+export default Kontak;
