4b19e6da7e969b0cc494ed65eda75a5c78f614e9
Nafhisa Nailah Husnah <133951870+nafhsnael@users.noreply.github.com>
2024-03-04 13:56:46 +0700

    Create beranda.js

---
+++ beranda.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './tampilan.css';
+
+class Beranda extends React.Component {
+    render() {
+        return(
+            <div>
+            <div class="tittle">
+              <h1>Welcome To Website Nafhs.nael</h1>
+            </div>
+            </div>
+            
+        )
+    }
+}
+
+export default Beranda;
